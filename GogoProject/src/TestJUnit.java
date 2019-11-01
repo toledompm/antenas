cb3ca6c1c77589c30a32fb4d3baaed5b2ab54869
toledompm <toledompm@gmail.com>
2019-11-01 20:16:46 -0300

    Creating a list of Professor objects inside Sistema Adding tests to TestJUnit

--- GogoProject/src/TestJUnit.java
+++ GogoProject/src/TestJUnit.java
@@ -2,28 +2,45 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class TestJUnit {
-
-	@Test
-	public void testJUnit() {
-		//fail("Not yet implemented");
+	//Declarando valores defaults para todas comparacoes com um objeto do tipo Professor
+	private String emailProfessor = "email@teste";
+	private String senhaProfessor = "senha";
+	private String matriculaProfessor = "mat";
+	
+	//inicializando um objeto do tipo Sistema
+	private Sistema sis = new Sistema();
+	//inicializando um objeto do tipo Professor
+	private Professor prof = new Professor();
+	
+	public TestJUnit() {
+		//Setando os valores do objeto Professor para os valores declarados
+		prof.setEmail(emailProfessor);
+		prof.setSenha(senhaProfessor);
+		prof.setMatricula(matriculaProfessor);
+		
+		//Cadastrando o Professor no sistema
+		sis.cadastrarProfessor(prof);
+		
+		//Executando os testes da classe professor
 		testProfessor();
+		
+		//Executando os testes da classe Sistema
+		testSistema();
+		
+	}
+	
+	@Test
+	public void testSistema() {
+		assertEquals(sis.buscarProfessorPorEmail(emailProfessor),prof);
+		assertEquals(sis.listarProfessores().contains(prof),true);
 		
 	}
 	
 	@Test
 	public void testProfessor() {
-		String novoEmail = "email@teste";
-		String novaSenha = "senha";
-		String novaMatricula = "mat";
-		Professor prof = new Professor();
-		
-		prof.setEmail(novoEmail);
-		prof.setSenha(novaSenha);
-		prof.setMatricula(novaMatricula);
-		
-		assertEquals(prof.getEmail(),novoEmail);
-		assertEquals(prof.getMatricula(),novaMatricula);
-		assertEquals(prof.getSenha(),novaSenha);
+		assertEquals(prof.getEmail(),emailProfessor);
+		assertEquals(prof.getMatricula(),matriculaProfessor);
+		assertEquals(prof.getSenha(),senhaProfessor);
 	}
 
 }
