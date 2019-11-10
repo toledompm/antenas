e650530c537a7944e81133e51bee1424f42eae94
evellin-amorim <evellinamorim@hotmail.com>
2019-11-10 12:41:55 -0300

    Alteraçao na classe entega e projeto gets e sets

--- GogoProject/src/Entrega.java
+++ GogoProject/src/Entrega.java
@@ -1,4 +1,34 @@
-
+import java.util.LinkedList;
 public class Entrega {
+	private String linkGit;
+	private String data;
+	private String linkGitOpcional;
+	private LinkedList<String> aluno = new LinkedList<String>();
+	
+	public String getLinkGit() {
+		return linkGit;
+	}
+	public void setLinkGit(String linkGit) {
+		this.linkGit = linkGit;
+	}
+	public String getData() {
+		return data;
+	}
+	public void setData(String data) {
+		this.data = data;
+	}
+	public String getLinkGitOpcional() {
+		return linkGitOpcional;
+	}
+	public void setLinkGitOpcional(String linkGitOpcional) {
+		this.linkGitOpcional = linkGitOpcional;
+	}
+	public LinkedList<String> getAluno() {
+		return aluno;
+	}
+	public void setAluno(LinkedList<String> aluno) {
+		this.aluno = aluno;
+	}
+	
 
 }
--- GogoProject/src/Projeto.java
+++ GogoProject/src/Projeto.java
@@ -1,4 +1,61 @@
-
+import java.util.LinkedList;
 public class Projeto {
-
+	private LinkedList<String> entregas = new LinkedList<String>();
+	private String descricao;
+	private String descricaoAvancada;
+	private String titulo;
+	private String link;
+	private String linkAvancado;
+	private String Empresario;
+	private String membroCadi;
+	public LinkedList<String> getEntregas() {
+		return entregas;
+	}
+	public void setEntregas(LinkedList<String> entregas) {
+		this.entregas = entregas;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public String getDescricaoAvancada() {
+		return descricaoAvancada;
+	}
+	public void setDescricaoAvancada(String descricaoAvancada) {
+		this.descricaoAvancada = descricaoAvancada;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getLink() {
+		return link;
+	}
+	public void setLink(String link) {
+		this.link = link;
+	}
+	public String getLinkAvancado() {
+		return linkAvancado;
+	}
+	public void setLinkAvancado(String linkAvancado) {
+		this.linkAvancado = linkAvancado;
+	}
+	public String getEmpresario() {
+		return Empresario;
+	}
+	public void setEmpresario(String empresario) {
+		Empresario = empresario;
+	}
+	public String getMembroCadi() {
+		return membroCadi;
+	}
+	public void setMembroCadi(String membroCadi) {
+		this.membroCadi = membroCadi;
+	}
+	
+	
 }
